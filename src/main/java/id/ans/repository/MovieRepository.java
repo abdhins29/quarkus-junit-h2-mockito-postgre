ea7c4fbb4fb67a7fa2e5b9265b1294e82362289f
abdhins29 <abdhins29@gmail.com>
2022-12-07 11:03:25 +0700

    upload

---
+++ src/main/java/id/ans/repository/MovieRepository.java
@@ -0,0 +1,15 @@
+package id.ans.repository;
+
+import id.ans.DTO.Movie;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+public class MovieRepository implements PanacheRepository<Movie> {
+
+    public List<Movie> findByCountry(String country) {
+        return list("SELECT m FROM Movie m WHERE m.country = ?1 ORDER BY id DESC", country);
+    }
+}
